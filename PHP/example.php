41eef96812e18113a0af13be03c64a69480f0422
Dennis Stengele <d.stengele@derintendant.de>
2013-10-02 23:27:56 +0200

    Updated example to work with current Google Maps version, removed auto zoom

--- PHP/example.php
+++ PHP/example.php
@@ -8,58 +8,38 @@
     // Enter your MobileMe username and password
     $ssm = new Sosumi('your_username', 'your_password');
     $loc = $ssm->locate();
-    
-    if(isset($_POST['btnSend']))
-    {
-        $alarm = isset($_POST['alarm']);
-        $ssm->sendMessage($_POST['msg'], $alarm);
-        header('Location: ' . $_SERVER['PHP_SELF']);
-    }
 ?>
-<!DOCTYPE html "-//W3C//DTD XHTML 1.0 Strict//EN" 
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
 <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
-    <title>Sosumi</title>
-    <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.8.0r4/build/reset-fonts-grids/reset-fonts-grids.css">
-    <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.8.0r4/build/base/base-min.css">
+    <title>Wo ist derintendant?</title>
+    <!-- <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.8.0r4/build/reset-fonts-grids/reset-fonts-grids.css">
+    <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.8.0r4/build/base/base-min.css"> -->
     <style type="text/css" media="screen">
         p { text-align:left; }
-        #map_canvas { width:640px; height:480px; border:1px solid #000; }
+        #map_canvas { width:100%; height:400px; border:1px solid #000; }
     </style>
-    <script src="http://maps.google.com/maps?file=api&amp;v=2&amp;key=<?PHP echo $google_maps_key; ?>&amp;sensor=false" type="text/javascript"></script>
+    <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?key=<?PHP echo $google_maps_key; ?>&amp;sensor=false"></script>
     <script type="text/javascript">
         function initialize() {
-            function zoomFit() {
-                newzoom = map.getBoundsZoomLevel(bounds);
-                newcenter = bounds.getCenter();
-                map.setCenter(newcenter, newzoom);
-            }
-
-            function createMarker(point, msg) {
-                bounds.extend(point);
-                var marker = new GMarker(point);
-                GEvent.addListener(marker, "click", function() {
-                    map.openInfoWindowHtml(point, msg);
-                });
-                zoomFit();
-                return marker;
-            }
-
-            if (GBrowserIsCompatible()) {
-                var bounds = new GLatLngBounds();
-
-                var map = new GMap2(document.getElementById("map_canvas"));
-                map.setUIToDefault();
-
-                var point = new GLatLng(<?PHP echo $loc['latitude']; ?>, <?PHP echo $loc['longitude']; ?>);
-                map.addOverlay(createMarker(point, "Your Location"));
-            }
+            var currentLocation = new google.maps.LatLng(<?PHP echo $loc['latitude']; ?>, <?PHP echo $loc['longitude']; ?>)
+            var mapOptions = {
+                center: currentLocation,
+                zoom: 14,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+            var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+            var marker = new google.maps.Marker({
+                position: currentLocation,
+                title: "derintendant"
+            })
+            marker.setMap(map);
         }
     </script>
 </head>
 <body onload="initialize()" onunload="GUnload()">
+    <!--
     <form action="" method="post">
         <p>
             <label for="msg">Message:</label>
@@ -69,6 +49,9 @@
             <input type="submit" name="btnSend" value="Send" id="btnSend">
         </p>
     </form>
+    -->
     <div id="map_canvas"></div>
+    <br />
+    <!-- <p>Genauigkeit: <?PHP echo $loc['accuracy']; ?></p> -->
 </body>
 </html>
